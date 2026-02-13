96c43dcd66865f09912cf6e632c8d11faae2df8d
zainnofal <zainnofalmessi@gmail.com>
2026-02-12 16:14:51 -0800

    Added server section and Distribution Plots

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ import numpy as np
 
 # Loading the Data
 df = pd.read_csv("data/raw/mars-weather.csv")
+df["terrestrial_date"] = pd.to_datetime(df["terrestrial_date"])
 
 # UI Section
 
@@ -30,8 +31,12 @@ app_ui = ui.page_fluid(
             ui.input_select(
                 "season",
                 None,
-                choices=["Autumn (ls=0)", "Winter (ls=90)",
-                         "Spring (ls=180)", "Summer (ls=270)"]
+                  choices={
+                    "Autumn (ls = 0)": 0,
+                    "Winter (ls = 90)": 90,
+                    "Spring (ls = 180)": 180,
+                    "Summer (ls = 270)": 270
+                }
             )
         ),
 
@@ -39,7 +44,9 @@ app_ui = ui.page_fluid(
             ui.h5("Terrestrial Date"),
             ui.input_date_range(
                 "date_range",
-                None
+                None,
+                start=df["terrestrial_date"].min(),
+                end=df["terrestrial_date"].max()
             )
         ),
     ),
@@ -85,5 +92,59 @@ app_ui = ui.page_fluid(
 
 
 )
+
+# Server
+
+def server(input, output, session):
+
+    @output
+    @render.text
+    def latest_sol():
+        return str(df["sol"].max())
+
+    @output
+    @render.text
+    def avg_min():
+        return f"{df['min_temp'].mean():.2f}"
+
+    @output
+    @render.text
+    def avg_max():
+        return f"{df['max_temp'].mean():.2f}"
+
+    @output
+    @render.text
+    def avg_pressure():
+        return f"{df['pressure'].mean():.2f}"
+
+    @output
+    @render.plot
+    def sol_plot():
+        plt.figure()
+        plt.hist(df["sol"], bins=20)
+        plt.title("Distribution of Sol")
+
+    @output
+    @render.plot
+    def ls_plot():
+        plt.figure()
+        plt.hist(df["ls"], bins=20)
+        plt.title("Distribution of Solar Longitude (ls)")
+
+    @output
+    @render.plot
+    def min_temp_plot():
+        plt.figure()
+        plt.hist(df["min_temp"], bins=20)
+        plt.title("Distribution of Minimum Temperature")
+
+    @output
+    @render.plot
+    def pressure_plot():
+        plt.figure()
+        plt.hist(df["pressure"], bins=20)
+        plt.title("Distribution of Pressure")
+
+app = App(app_ui, server)
 
 
